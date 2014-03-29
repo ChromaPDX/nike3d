8204a211c13af0c3e584a685296a07f1617a49b5
Chroma Developer <leif@structuresound.com>
2014-03-28 17:19:38 -0700

    bordermask

--- src/BoardLocation.h
+++ src/BoardLocation.h
@@ -8,6 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_OPTIONS(UInt16, BorderMask) {
+    
+    BorderMaskNone = 0,
+    
+    BorderMaskLeft = 1 << 1,
+    BorderMaskRight = 1 << 2,
+    BorderMaskTop = 1 << 3,
+    BorderMaskBottom = 1 << 4,
+    
+    BorderMaskUR = BorderMaskTop | BorderMaskRight,
+    BorderMaskUL = BorderMaskTop | BorderMaskLeft,
+    BorderMaskBR = BorderMaskBottom | BorderMaskRight,
+    BorderMaskBL = BorderMaskBottom | BorderMaskLeft,
+    
+    BorderMaskVertical = BorderMaskLeft | BorderMaskRight,
+    BorderMaskHorizontal = BorderMaskTop | BorderMaskBottom,
+    BorderMask3top = BorderMaskLeft | BorderMaskTop | BorderMaskRight,
+    BorderMask3bottom = BorderMaskLeft | BorderMaskBottom | BorderMaskRight,
+    BorderMask3left = BorderMaskLeft | BorderMaskBottom | BorderMaskTop,
+    BorderMask3right = BorderMaskRight | BorderMaskBottom | BorderMaskTop,
+
+    BorderMaskAll = BorderMaskLeft | BorderMaskRight | BorderMaskBottom | BorderMaskTop,
+    
+};
+
 @interface BoardLocation : NSObject <NSCopying, NSCoding>
 {
 
